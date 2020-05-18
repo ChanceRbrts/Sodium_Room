16e5f5a25a14f041cfc8e54d3724243f1c401699
PlatPlat48 <roberts.chance@gmail.com>
2020-05-17 21:41:22 -0600

    Save Vals now in Game State
    Saved values are now in the game state map. Now, we need to come up
    with a system to load/save files!

--- utils/gameState.cpp
+++ utils/gameState.cpp
@@ -6,3 +6,68 @@ std::map<std::string, saveVar> GameState::emptyMap(){
     std::map<std::string, saveVar> retMap;
     return retMap;
 }
+
+bool GameState::hasValue(std::string key){
+    std::map<std::string, saveVar>::iterator it = stateMap.find(key);
+    return it != stateMap.end();
+}
+
+int GameState::loadFile(std::string fileName){
+
+}
+
+int GameState::saveFile(std::string fileName){
+
+}
+
+
+int GameState::getType(std::string key){
+    if (!hasValue(key)) return SAV_ERR;
+    saveVar sV = stateMap.at(key);
+    return sV.type;
+}
+
+bool GameState::getSaveB(std::string key){
+    if (!hasValue(key)) return false;
+    saveVar sV = stateMap.at(key);
+    if (sV.type == SAV_BOOL) return sV.data.b;
+    if (sV.type == SAV_INT) return sV.data.i != 0;
+    if (sV.type == SAV_DOUBLE) return (sV.data.d != 0);
+    return false;
+}
+
+int GameState::getSaveI(std::string key){
+    if (!hasValue(key)) return 0;
+    saveVar sV = stateMap.at(key);
+    if (sV.type == SAV_BOOL) return sV.data.b ? 1 : 0;
+    if (sV.type == SAV_INT) return sV.data.i;
+    if (sV.type == SAV_DOUBLE) return (int)(sV.data.d);
+    return 0;
+}
+
+double GameState::getSaveD(std::string key){
+    if (!hasValue(key)) return 0;
+    saveVar sV = stateMap.at(key);
+    if (sV.type == SAV_BOOL) return sV.data.b ? 1 : 0;
+    if (sV.type == SAV_INT) return (double)(sV.data.i);
+    if (sV.type == SAV_DOUBLE) return sV.data.d;
+    return 0;
+}
+
+void GameState::setSaveB(std::string key, bool b){
+    saveVar sV = (saveVar){ true, SAV_BOOL, b };
+    if (hasValue(key)) stateMap[key] = sV;
+    else stateMap.insert({key, sV});
+}
+
+void GameState::setSaveI(std::string key, int i){
+    saveVar sV = (saveVar){ true, SAV_INT, i };
+    if (hasValue(key)) stateMap[key] = sV;
+    else stateMap.insert({key, sV});
+}
+
+void GameState::setSaveD(std::string key, double d){
+    saveVar sV = (saveVar){ true, SAV_DOUBLE, d };
+    if (hasValue(key)) stateMap[key] = sV;
+    else stateMap.insert({key, sV});
+}
--- utils/gameState.h
+++ utils/gameState.h
@@ -3,24 +3,21 @@
 
 #include <string>
 #include <map>
+#include <cmath>
 
 #define SAV_ERR -1
 #define SAV_BOOL 0
 #define SAV_INT 1
-#define SAV_FLOAT 2
-#define SAV_STRING 3
-
-union saveData{
-    bool b;
-    int i;
-    float f;
-    std::string str;
-};
+#define SAV_DOUBLE 2
 
 struct saveVar{
     bool save;
     int type;
-    saveData data;
+    union {
+        bool b;
+        int i;
+        double d;
+    } data;
 };
 
 /**
@@ -32,6 +29,7 @@ struct saveVar{
 class GameState{
     private:
         static std::map<std::string, saveVar> stateMap;
+        static bool hasValue(std::string key);
     public:
         static std::map<std::string, saveVar> emptyMap();
         static int loadFile(std::string fileName);
@@ -40,16 +38,13 @@ class GameState{
         static saveVar getSave(std::string key);
         static bool getSaveB(std::string key);
         static int getSaveI(std::string key);
-        static float getSaveF(std::string key);
-        static std::string getSaveS(std::string key);
+        static double getSaveD(std::string key);
         static void setSave(std::string key, bool value){ setSaveB(key, value); };
         static void setSaveB(std::string, bool value);
         static void setSave(std::string key, int value){ setSaveI(key, value); };
         static void setSaveI(std::string key, int value);
-        static void setSave(std::string key, float value){ setSaveF(key, value); };
-        static void setSaveF(std::string key, float value);
-        static void setSave(std::string key, std::string value){ setSaveS(key, value); };
-        static void setSaveS(std::string key, std::string value);
+        static void setSave(std::string key, double value){ setSaveD(key, value); };
+        static void setSaveD(std::string key, double value);
 };
 
 #endif
